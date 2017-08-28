70a887681a9e0d37b6406a8b344cfa9a2125d3fd
Chithralekha <chithra@altiussystems.com>
2017-08-28 15:07:30 -0400

    ui sortable, taskboard from cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,12 @@ import {AppviewsModule} from "./views/appviews/appviews.module";
 
 // App modules/components
 import {LayoutsModule} from "./components/common/layouts/layouts.module";
+import { TaskboardComponent } from './views/taskboard/taskboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskboardComponent
   ],
   imports: [
     BrowserModule,

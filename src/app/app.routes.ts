a75b1bf12ba7dfbbb50fb50c7c6bfd5f80114c2b
Chithralekha <chithra@altiussystems.com>
2017-09-14 09:49:01 -0400

    Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,48 +6,51 @@ import {Dashboard3Component} from "./views/dashboards/dashboard3.component";
 import {Dashboard4Component} from "./views/dashboards/dashboard4.component";
 import {Dashboard41Component} from "./views/dashboards/dashboard41.component";
 import {Dashboard5Component} from "./views/dashboards/dashboard5.component";
-
 import {TaskboardComponent} from "./views/taskboard/taskboard.component";
-
 import {StarterViewComponent} from "./views/appviews/starterview.component";
 import {LoginComponent} from "./views/login/login.component";
-
 import {BlankLayoutComponent} from "./components/common/layouts/blankLayout.component";
 import {BasicLayoutComponent} from "./components/common/layouts/basicLayout.component";
 import {TopNavigationLayoutComponent} from "./components/common/layouts/topNavigationlayout.component";
+
 import { WorkingSetResolver } from './views/dashboards/workingSet-resolver.service';
 import { AuthGuard} from '../app/core/auth-guard.service';
+
 export const ROUTES:Routes = [
   // Main redirect
-  {path: '', redirectTo: 'dashboards/dashboard1', pathMatch: 'full'},
+  { path: '', redirectTo: 'dashboards/dashboard1', pathMatch: 'full'},
 
   // App views
   {
     path: 'taskboard', component: BasicLayoutComponent,
-     runGuardsAndResolvers: 'paramsOrQueryParamsChange',
-     canActivate: [AuthGuard],
+    runGuardsAndResolvers: 'paramsOrQueryParamsChange',
+    canActivate: [AuthGuard],
     children: [
-      {path: 'taskboard/:filterId', component: TaskboardComponent}
-      ]
-  },
-  {
-    path: 'dashboards', component: BasicLayoutComponent,
-    children: [
-      {path: 'dashboard1', component: Dashboard1Component, resolve: {
-          workingSets: WorkingSetResolver
-        },
-      canActivate: [AuthGuard]},
+      { path: 'taskboard/:filterId', 
+        component: TaskboardComponent} ]
+      },
+      {
+        path: 'dashboards', component: BasicLayoutComponent,
+        children: [
+          {
+            path: 'dashboard1', 
+            component: Dashboard1Component, 
+            resolve: { workingSets: WorkingSetResolver },
+            canActivate: [AuthGuard]
+          },
       {path: 'dashboard2', component: Dashboard2Component},
       {path: 'dashboard3', component: Dashboard3Component},
       {path: 'dashboard4', component: Dashboard4Component},
-      {path: 'dashboard5', component: Dashboard5Component}
-    ]
+      {path: 'dashboard5', component: Dashboard5Component} ]
   },
   {
-    path: 'dashboards', component: TopNavigationLayoutComponent,
+    path: 'dashboards', 
+    component: TopNavigationLayoutComponent,
     children: [
-      {path: 'dashboard41', component: Dashboard41Component}
-    ]
+      {
+        path: 'dashboard41', 
+        component: Dashboard41Component
+      } ]
   },  
   {
     path: '', component: BlankLayoutComponent,

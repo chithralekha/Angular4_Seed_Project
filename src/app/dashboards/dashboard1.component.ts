0945bd023b7cc8316a6c78f611b8b59bf18c1204
Chithralekha <chithra@altiussystems.com>
2017-09-25 15:14:55 -0400

    Dashboard

--- src/app/dashboards/dashboard1.component.ts
+++ src/app/dashboards/dashboard1.component.ts
@@ -77,7 +77,30 @@ export class Dashboard1Component implements OnInit {
         ]
         }
     });
- } 
+    let chartBcp = c3.generate({
+      data: {
+        columns: [
+          ['data', 91.4]
+          ],
+          type: 'gauge',
+          onclick: function (d, i) { console.log("onclick", d, i); },
+          onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+          onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+        },
+        gauge: {
+          label: {
+            format: function(value, ratio) {
+              return value;
+            },
+            show: false // to turn off the min/max labels.
+          },
+          min: 0, // 0 is default, //can handle negative min e.g. vacuum / voltage / current flow / rate of change
+          max: 100, // 100 is default
+          units: ' %',
+          width: 39 // for adjusting arc thickness
+        },
+      });
+    } 
  determineColor(compliance:number) {
    var complianceColorCode = null;
         switch(true)
